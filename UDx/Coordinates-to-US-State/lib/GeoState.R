b19708ba1eac188a504f66dcc188a18f636d6a15
Igor Marchenko <imarchenko@Newton.local>
2013-10-30 16:36:31 -0400

    Fixed a bug (now works with more than one set of input coords) Reports in an abbreviated state name now instead of full name

--- UDx/Coordinates-to-US-State/lib/GeoState.R
+++ UDx/Coordinates-to-US-State/lib/GeoState.R
@@ -2,33 +2,33 @@
 # Converts a longitude and latitude value into a United States state.
 #
 # R Dependencies:
-#	sp, maps, and maptools
+#       sp, maps, and maptools
 
 library(sp)
 library(maps)
 library(maptools)
 
 GeoState <- function(coords) {
-	# Computes the USA state based on a longitude and latitude coordinate
-	#
-	# Args:
-	#   coords: A dataframe that contains two items, longitude and latitude coordinates
-	#
-	# Returns:
-	#   A US state if coordinates are within the USA bounds, otherwise return null
-	states <- map('state', fill=TRUE, col="transparent", plot=FALSE)
-	IDs <- sapply(strsplit(states$names, ":"), function(x) x[1])
-	statesSP <- map2SpatialPolygons(states, IDs=IDs, proj4string=CRS("+proj=longlat +datum=wgs84"))
+        # Computes the USA state based on a longitude and latitude coordinate
+        #
+        # Args:
+        #   coords: A dataframe that contains two items, longitude and latitude coordinates
+        #
+        # Returns:
+        #   A US state if coordinates are within the USA bounds, otherwise return null
+        states <- map('state', fill=TRUE, col="transparent", plot=FALSE)
+        IDs <- sapply(strsplit(states$names, ":"), function(x) x[1])
+        statesSP <- map2SpatialPolygons(states, IDs=IDs, proj4string=CRS("+proj=longlat +datum=wgs84"))
 
-	pointSP <- SpatialPoints(coords, proj4string=CRS("+proj=longlat +datum=wgs84"))
-	index <-over(pointSP, statesSP)
+        pointsSP <- SpatialPoints(coords, proj4string=CRS("+proj=longlat +datum=wgs84"))
+        indices <- over(pointsSP, statesSP)
 
-	stateNames <- sapply(statesSP@polygons, function(x) x@ID)
-	state.abb[grep(sub(":.*", "", stateNames[index]), state.name, ignore.case = TRUE)]
+        stateNames <- sapply(statesSP@polygons, function(x) x@ID)
+        sapply(stateNames[indices], function(x) ifelse(is.na(x), x, (state.abb[grep(sub(":.*", "", x), state.name, ignore.case = TRUE)])))
 }
 
 GeoStateFactory <- function() {
-	# A Vertica Scalar Function UDx Factory
-	# Takes two numeric values (longitude, latitude) and returns a varchar value
-	list(name=GeoState,udxtype=c("scalar"),intype=c("numeric","numeric"), outtype=c("varchar"))
+        # A Vertica Scalar Function UDx Factory
+        # Takes two numeric values (longitude, latitude) and returns a varchar value
+        list(name=GeoState,udxtype=c("scalar"),intype=c("float","float"), outtype=c("varchar"))
 }
